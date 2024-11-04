3a18f2bcf1f3012b38c824ded87e72357b7a6f6c
Ruben Bensimon <rubensbensimon@Gmail.com>
2024-11-04 15:06:34 +0200

    welcome function

---
+++ stage0/Program5424.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stage0
+{
+    partial class Program5424
+    {
+        private static void Welcome5424()
+        {
+            Console.WriteLine("I'm also here!");
+
+        }
+
+    }
+
+
+}
