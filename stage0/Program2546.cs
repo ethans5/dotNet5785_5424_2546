e86aaeef43b513792b5f6dfffabb37a77a6c90b1
ethansarfati <esarfati@g.jct.ac.il>
2024-11-04 14:58:17 +0200

    initial code + welcome functions

---
+++ stage0/Program2546.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2546();
+            Welcome5424();
+            Console.ReadKey();
+
+        }
+        static partial void Welcome5424();
+
+        private static void Welcome2546()
+        {
+            Console.Write("Enter your name:");
+            string userName = Console.ReadLine()!;
+            Console.WriteLine("{0},welcome to my first console application", userName);
+        }
+    }
+}
